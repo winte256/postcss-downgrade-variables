c934a6bbb7d4012454f1a8f699b839b174ec97cf
Michael <m.krutihin@yandex.ru>
2017-10-26 15:13:06 +0300

    fix bug

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const postcss = require('postcss');
 
 const replaceToDefaultColor = (css, colors = {}) =>
   css.replace(/var\(([\s\S]+?)\)/g, (match, p1) => colors[p1] || match);
+const variablesColors = {};
 
 module.exports = postcss.plugin('postcss-downgrade-variables', (options = {}) => (css) => {
-  const variablesColors = {};
-
   css.walkRules(':root', (rule) => {
     rule.walkDecls((decl) => {
       variablesColors[decl.prop] = decl.value;
     });
   });
+
   css.walkDecls((decl) => {
     if (/var\(--/.test(decl.value)) {
       decl.before({
