58b1c0c3d74baa64a1e8ee2a5da968273edf259f
Michael <m.krutihin@yandex.ru>
2017-10-27 13:53:59 +0300

    add tests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const replaceToDefaultColor = (css, colors = {}) =>
   css.replace(/var\(([\s\S]+?)\)/g, (match, p1) => colors[p1] || match);
 const variablesColors = {};
 
-module.exports = postcss.plugin('postcss-downgrade-variables', (options = {}) => (css) => {
+module.exports = postcss.plugin('postcss-downgrade-variables', () => (css) => {
   css.walkRules(':root', (rule) => {
     rule.walkDecls((decl) => {
       variablesColors[decl.prop] = decl.value;
@@ -13,6 +13,11 @@ module.exports = postcss.plugin('postcss-downgrade-variables', (options = {}) =>
 
   css.walkDecls((decl) => {
     if (/var\(--/.test(decl.value)) {
+      const already = decl.parent.some((i) => i.type === 'decl'
+        && i.prop === decl.prop
+        && i.value === replaceToDefaultColor(decl.value, variablesColors));
+      if (already) return;
+
       decl.cloneBefore({ value: replaceToDefaultColor(decl.value, variablesColors) });
     }
   });
